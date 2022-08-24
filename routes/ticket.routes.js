b47384ba9f20dd3ea8e61fd72ff5c2b6c0312cc1
d02ev <vikramadityapratapsingh98@gmail.com>
2022-08-24 20:05:21 +0530

    Update ticket.routes.js

--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -1,7 +1,8 @@
 const Route = require('express').Router();
 const TicketController = require('../controllers/ticket.controller');
 const CreateTicketAuth = require('../middleware/ticket_create_auth');
-const CloseTicketAuth = require('../middleware/ticket_close_auth');
+const CloseTicketAdminAuth = require('../middleware/ticket_close_admin_auth');
+const CloseTicketEmpAuth = require('../middleware/ticket_close_emp_auth');
 const DeleteTicketAuth = require('../middleware/ticket_delete_auth');
 
 Route.post('/new', CreateTicketAuth, TicketController.apiCreateTicket);
@@ -9,7 +10,7 @@ Route.get('/all', TicketController.apiGetAllTickets);
 Route.get('/bystatus', TicketController.apiGetTicketsByStatus);
 Route.get('/bytitle', TicketController.apiGetTicketsByTitle);
 Route.get('/bypriority', TicketController.apiGetTicketsByPriority);
-Route.post('/close', CloseTicketAuth, TicketController.apiCloseTicket);
+Route.post('/close', CloseTicketEmpAuth, CloseTicketAdminAuth, TicketController.apiCloseTicket);
 Route.post('/delete', DeleteTicketAuth, TicketController.apiDeleteTicket);
 
 module.exports = Route;
